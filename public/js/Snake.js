38f56a13972415634c92ff67a4d51a0a1e6bc595
Aecxlol <aubry_damien@orange.fr>
2021-01-16 12:09:19 +0100

    fixed an issue if the key was pressed twice or more

--- public/js/Snake.js
+++ public/js/Snake.js
@@ -6,7 +6,7 @@ class Snake {
         this.snakeShape         = null;
         this.defaultSnakeWidth  = 10;
         this.defaultSnakeHeight = 10;
-        this.refreshRate = 300;
+        this.refreshRate = 100;
 
         this.keybinds = {
             l: "ArrowLeft",
@@ -21,6 +21,13 @@ class Snake {
         }
 
         this.interval = {
+            l: null,
+            r: null,
+            t: null,
+            b: null
+        }
+
+        this.keybindsPressed = {
             l: null,
             r: null,
             t: null,
@@ -67,55 +74,57 @@ class Snake {
             switch (e.code) {
 
                 case this.keybinds.l:
-
-                    console.log(this.direction.length);
-                    this.movesCount += 1;
-                    if(this.movesCount >= 1) {
-                        this.hasDirectionChanged = true;
-                        this.direction.push('l');
+                    // prevent the x axis to still iterate
+                    // if the key is pressed twice or more
+                    if(!this.keybindsPressed.l){
+                        this.movesCount += 1;
+                        if(this.movesCount >= 1) {
+                            this.hasDirectionChanged = true;
+                            this.direction.push('l');
+                        }
+                        this.interval.l = setInterval(() => {
+                            this.snakeShape.updateCanvas("l")
+                        }, this.refreshRate);
                     }
-                    this.interval.l = setInterval(() => {
-                        this.snakeShape.updateCanvas("l")
-                    }, this.refreshRate);
                 break;
 
                 case this.keybinds.r:
-
-                    console.log(this.direction.length);
-                    this.movesCount += 1;
-                    if(this.movesCount >= 1) {
-                        this.hasDirectionChanged = true;
-                        this.direction.push('r');
+                    if(!this.keybindsPressed.r){
+                        this.movesCount += 1;
+                        if(this.movesCount >= 1) {
+                            this.hasDirectionChanged = true;
+                            this.direction.push('r');
+                        }
+                        this.interval.r = setInterval(() => {
+                            this.snakeShape.updateCanvas("r")
+                        }, this.refreshRate);
                     }
-                    this.interval.r = setInterval(() => {
-                        this.snakeShape.updateCanvas("r")
-                    }, this.refreshRate);
                 break;
 
                 case this.keybinds.t:
-
-                    console.log(this.direction.length);
-                    this.movesCount += 1;
-                    if(this.movesCount >= 1) {
-                        this.hasDirectionChanged = true;
-                        this.direction.push('t');
+                    if(!this.keybindsPressed.t){
+                        this.movesCount += 1;
+                        if(this.movesCount >= 1) {
+                            this.hasDirectionChanged = true;
+                            this.direction.push('t');
+                        }
+                        this.interval.t = setInterval(() => {
+                            this.snakeShape.updateCanvas("t")
+                        }, this.refreshRate);
                     }
-                    this.interval.t = setInterval(() => {
-                        this.snakeShape.updateCanvas("t")
-                    }, this.refreshRate);
                 break;
 
                 case this.keybinds.b:
-
-                    console.log(this.direction.length);
-                    this.movesCount += 1;
-                    if(this.movesCount >= 1) {
-                        this.hasDirectionChanged = true;
-                        this.direction.push('b');
+                    if(!this.keybindsPressed.b){
+                        this.movesCount += 1;
+                        if(this.movesCount >= 1) {
+                            this.hasDirectionChanged = true;
+                            this.direction.push('b');
+                        }
+                        this.interval.b = setInterval(() => {
+                            this.snakeShape.updateCanvas("b")
+                        }, this.refreshRate);
                     }
-                    this.interval.b = setInterval(() => {
-                        this.snakeShape.updateCanvas("b")
-                    }, this.refreshRate);
                 break;
             }
         });
@@ -139,17 +148,25 @@ class Snake {
 
             updateCanvas: (direction) => {
                 // clear the trail left by the snake
+                // this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
                 this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
                 this.snakeShape.drawSnake();
                 switch (direction) {
                     // LEFT : X AXIS
                     case "l":
+                        this.keybindsPressed.l = true;
+                        this.keybindsPressed.r = false;
+                        this.keybindsPressed.t = false;
+                        this.keybindsPressed.b = false;
+                        // if it's not the first move
                         if(this.movesCount > 1) {
                             // stop the interval of the previous move
                             clearInterval(this.interval[this.direction[this.movesCount - 2]]);
                         }
 
+                        // if the snake doesn't hit the edge of the canvas
                         if(this.snakeShape.x > -this.defaultSnakeWidth && this.snakeShape.x < this.canvas.width) {
+                            //make it move
                             this.snakeShape.x -= this.defaultSnakeWidth;
                         }else {
                             this._endTheGame();
@@ -158,13 +175,18 @@ class Snake {
 
                     // RIGHT : X AXIS
                     case "r":
+                        this.keybindsPressed.l = false;
+                        this.keybindsPressed.r = true;
+                        this.keybindsPressed.t = false;
+                        this.keybindsPressed.b = false;
                         if(this.movesCount > 1) {
                             // stop the interval of the previous move
                             clearInterval(this.interval[this.direction[this.movesCount - 2]]);
                         }
 
                         if(this.snakeShape.x > -this.defaultSnakeWidth && this.snakeShape.x < this.canvas.width) {
-                            this.snakeShape.x += this.defaultSnakeWidth;
+                            console.log(this.lastPosition.y);
+                                this.snakeShape.x += this.defaultSnakeWidth;
                         }else {
                             this._endTheGame();
                         }
@@ -172,6 +194,10 @@ class Snake {
 
                     // TOP : Y AXIS
                     case "t":
+                        this.keybindsPressed.l = false;
+                        this.keybindsPressed.r = false;
+                        this.keybindsPressed.t = true;
+                        this.keybindsPressed.b = false;
                         if(this.movesCount > 1) {
                             // stop the interval of the previous move
                             clearInterval(this.interval[this.direction[this.movesCount - 2]]);
@@ -186,6 +212,10 @@ class Snake {
 
                     // BOTTOM : Y AXIS
                     case "b":
+                        this.keybindsPressed.l = false;
+                        this.keybindsPressed.r = false;
+                        this.keybindsPressed.t = false;
+                        this.keybindsPressed.b = true;
                         if(this.movesCount > 1) {
                             // stop the interval of the previous move
                             clearInterval(this.interval[this.direction[this.movesCount - 2]]);
@@ -200,7 +230,6 @@ class Snake {
                 }
             }
         };
-
         this.snakeShape.drawSnake();
     }
 
